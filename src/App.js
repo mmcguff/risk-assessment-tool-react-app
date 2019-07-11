0c97a9be9d76c7d58aefe850b377e8d25ffa1f72
Matthew McGuff <matthew.mcguff@walmart.com>
2019-07-10 21:43:54 -0500

    Made change to prep app for running in wp.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MainForm from './components/MainForm';
+
 import './App.css';
 import './bulma.css';
-import MainForm from './components/MainForm';
 
 class App extends Component {
 
   render() {
     return(
-        <MainForm />
+      <BrowserRouter>
+        <MainForm/>
+      </BrowserRouter>
     )
   }
 }
